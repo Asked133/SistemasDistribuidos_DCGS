4c35c1d20f17914581086c87b147b6df30174a85
Asked133 <dcgomez@outlook.com>
2025-09-09 06:57:43 -0600

    Feat: Created Update and GetMobsByName

---
+++ MobsApi/Dtos/UpdateMobDto.cs
@@ -0,0 +1,19 @@
+using System.Runtime.Serialization;
+
+namespace MobApi.Dtos;
+
+[DataContract(Name = "UpdateMobDto", Namespace = "http://mob-api/mob-service")]
+public class UpdateMobDto
+{
+    [DataMember(Name = "Id", Order = 1)]
+    public Guid Id { get; set; }
+
+    [DataMember(Name = "Name", Order = 2)]
+    public string Name { get; set; }
+
+    [DataMember(Name = "Type", Order = 3)]
+    public string Type { get; set; }
+
+    [DataMember(Name = "Stats", Order = 4)]
+    public StatsDto Stats { get; set; }
+}
